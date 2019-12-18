3dea24686511451074a658f308a7ff90b7738fb0
James Jensen <jamesarthurjensen@gmail.com>
2019-12-18 13:39:10 +0800

    Remove redundant `getInitialProps`i
    Not only did the `getInitialProps` method in `_app.tsx` not do anything,
    but it also disabled Automatic Static Optimisation.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 import App from "next/app";
-import {
-  AppContextType,
-  AppInitialProps,
-} from "next/dist/next-server/lib/utils";
-import { Router } from "next/dist/client/router";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import GlobalStyle from "../components/global-style";
@@ -12,19 +7,6 @@ import GlobalStyle from "../components/global-style";
 config.autoAddCss = false;
 
 export default class CustomApp extends App {
-  static async getInitialProps({
-    Component,
-    ctx,
-  }: AppContextType<Router>): Promise<AppInitialProps> {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render(): JSX.Element {
     const { Component, pageProps } = this.props;
 
