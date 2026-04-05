bc35def1b8a65b9f7dca05e2a814fdd6d754427e
James Jensen <jamesarthurjensen@gmail.com>
2026-04-05 22:32:20 +0800

    Clean

--- components/emph.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { primaryColor, secondaryColor } from "../utils/theme";
-
-const Emph = styled.span<{ secondary?: boolean }>`
-  color: ${({ secondary }): string =>
-    secondary ? secondaryColor : primaryColor};
-`;
-
-export default Emph;
